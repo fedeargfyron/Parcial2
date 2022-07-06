305960871e6a0eb9067e459b947cf80b44e61b47
fedeargfyron <fedemgs15@gmail.com>
2022-07-06 16:58:41 -0300

    Se genera dashboard dinámico

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,3 +1,5 @@
+let headers = ["id", "email", "phone", "username", "website"]
+
 const generateError = (err) => {
     console.log(err)
 }
@@ -6,15 +8,36 @@ const getInfo = async () => {
     try {
         let response = await fetch("https://basic-server-one.vercel.app/users", { method: "GET"})
         let body = await response.json();
-        generateDashboard(body);
+        generateDashboard(body.data);
     }
     catch(err){
         generateError(err);
     }
 }
 
-const generateDashboard = (response) => {
-    console.log(response)
+const createHeaderRow = () => {
+    let firstRow = document.createElement("tr");
+    headers.forEach(x => {
+        let th = document.createElement("th");
+        th.innerHTML = x.toUpperCase();
+        firstRow.appendChild(th);
+    })
+    return firstRow;
+}
+
+const generateDashboard = (data) => {
+    let table = document.getElementById("table");
+    table.appendChild(createHeaderRow());
+    data.forEach(user => {
+        let newRow = document.createElement("tr");
+        headers.forEach(att => {
+            let td = document.createElement("td");
+            td.innerHTML = user[att];
+            newRow.appendChild(td);
+        })
+        table.appendChild(newRow);
+    })
+
 }
 
 window.onload = async () => {
